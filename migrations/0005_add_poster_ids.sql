97af26fe3278383ce4dfc7ca708a087354846801
KuuminKochi <yesimkurisu@gmail.com>
2026-08-09 00:13:43 +0800

    style: format poster ID migration

--- migrations/0005_add_poster_ids.sql
+++ migrations/0005_add_poster_ids.sql
@@ -1,5 +1,3 @@
-ALTER TABLE threads
-ADD COLUMN poster_id TEXT NOT NULL DEFAULT 'Anonymous';
+ALTER TABLE threads ADD COLUMN poster_id TEXT NOT NULL DEFAULT 'Anonymous';
 
-ALTER TABLE replies
-ADD COLUMN poster_id TEXT NOT NULL DEFAULT 'Anonymous';
+ALTER TABLE replies ADD COLUMN poster_id TEXT NOT NULL DEFAULT 'Anonymous';
